5b5b4d22b0aa79c48b72939e46e82c21c779a54e
Seiya Kobayashi <seiyak@bu.edu>
2018-03-19 21:10:01 -0400

    second commit, error fixed.

--- zlib_sample.c
+++ zlib_sample.c
@@ -1,12 +1,13 @@
 /*
  * Author: Seiya Kobayashi
- * Date: Feb.26, 2018
+ * Date: Feb.26, 2018 (Updated on Mar.19, 2018)
  * Description: Simple implementation of zlib deflation/inflation in c.
  *              Wrote mainly for solidifying my understanding of zlib,
  *              the most widely-used library for data compression.
  *              This would be an important milestone to be able to write
- *              git implementation, which uses zlib to (de)compress
- *              its objects (such as blob).
+ *              git implementation, which uses zlib to compress/uncompress
+ *              its objects (such as blobs).
+ * Reference: https://www.zlib.net/manual.html
  */
 
 #include <stdio.h>
@@ -14,73 +15,76 @@
 #include <string.h>
 #include <zlib.h>
 
-// Compress "a" into "b"
-void _compress(char *a, char *b)
+// Compress "src" into "dest" ("a" into "b" in this example)
+void _compress(char *src, uInt srcSize, char *dest, uInt destSize)
 {
-  z_stream zd;
-  /* Set zalloc and zfree to Z_NULL, so that deflateInit upadates them
+    z_stream zd;
+    /* Set zalloc and zfree to Z_NULL, so that deflateInit upadates them
      to use default allocation functions.
-   */
-  zd.zalloc = Z_NULL;
-  zd.zfree = Z_NULL;
-  zd.opaque = Z_NULL;
-
-  // Set "a" as an input and "b" as an output
-  zd.next_in = (Bytef *)a;
-  zd.avail_in  = (uInt)strlen(a)+1;      // String + Terminator
-  zd.next_out = (Bytef *)b;
-  zd.avail_out = (uInt)sizeof(b);      // Size of the output
-
-  if (deflateInit(&zd, Z_BEST_COMPRESSION) != Z_OK)
-    perror("deflateInit failed");
-  deflate(&zd, Z_FINISH);
-  if (deflateEnd(&zd) != Z_OK)
-    perror("deflateEnd failed");
-
-  // Prints the compressed string (and its length) stored in "b"
-  printf("Compressed string:\n\t%s\n\twith its length of %lu\n", b, zd.total_out);
+     */
+    zd.zalloc = Z_NULL;
+    zd.zfree = Z_NULL;
+    zd.opaque = Z_NULL;
+    
+    // Set "src" as an input and "dest" as an output
+    zd.next_in = (Bytef *)src;
+    zd.avail_in  = srcSize+1;      // String + Terminator
+    zd.next_out = (Bytef *)dest;
+    zd.avail_out = destSize;      // Size of the output
+    
+    if (deflateInit(&zd, Z_BEST_COMPRESSION) != Z_OK)
+        perror("deflateInit failed");
+    deflate(&zd, Z_FINISH);
+    if (deflateEnd(&zd) != Z_OK)
+        perror("deflateEnd failed");
+    
+    // Prints the compressed string (and its length) stored in "dest"
+    printf("Compressed string:\n\t%s\n\twith its length of %lu\n", dest, zd.total_out);
 }
 
-// Decompress "b" to "c"
-void _decompress(char *a, char *b)
+// Uncompress "src" to "dest" ("b" into "c" in this example)
+void _uncompress(char *src, uInt srcSize, char *dest, uInt destSize)
 {
-  z_stream zi;
-  /* Set zalloc and zfree to Z_NULL, so that inflateInit upadates them
+    z_stream zi;
+    /* Set zalloc and zfree to Z_NULL, so that inflateInit upadates them
      to use default allocation functions.
-   */
-  zi.zalloc = Z_NULL;
-  zi.zfree = Z_NULL;
-  zi.opaque = Z_NULL;
-
-  // Set "a" as an input and "b" as an output
-  zi.next_in = (Bytef *)a;
-  zi.avail_in = (uInt)((char *)zi.next_out - b);      // Size of the input
-  zi.next_out = (Bytef *)b;
-  zi.avail_out = (uInt)sizeof(b);      // Size of the output
-
-  if (inflateInit(&zi) != Z_OK)
-    perror("inflateInit failed");
-  inflate(&zi, Z_NO_FLUSH);
-  if (inflateEnd(&zi) != Z_OK)
-    perror("inflateEnd failed");
-
-  // Prints the decompressed string (and its length) stored in "b"
-  printf("Decompressed string:\n\t%s\n\twith its length of %lu\n", b, strlen(b));
+     */
+    zi.zalloc = Z_NULL;
+    zi.zfree = Z_NULL;
+    zi.opaque = Z_NULL;
+    
+    // Set "src" as an input and "dest" as an output
+    zi.next_in = (Bytef *)src;
+    zi.avail_in = (uInt)((char *)zi.next_out - dest);      // Size of the input
+    zi.next_out = (Bytef *)dest;
+    zi.avail_out = destSize;      // Size of the output
+    
+    if (inflateInit(&zi) != Z_OK)
+        perror("inflateInit failed");
+    inflate(&zi, Z_NO_FLUSH);
+    if (inflateEnd(&zi) != Z_OK)
+        perror("inflateEnd failed");
+    
+    // Prints the uncompressed string (and its length) stored in "dest"
+    printf("Uncompressed string:\n\t%s\n\twith its length of %lu\n", dest, strlen(dest));
 }
 
 int main(int argc, char* argv[])
 {
-  // Initial string length = 34
-  char a[40] = "Hello, my name is Seiya Kobayashi.";
-  // Placeholder for the compressed "a"
-  char b[40];
-  // Placeholder for the decompressed "b"
-  char c[40];
-  // Prints the initial string (and its length) stored in "a"
-  printf("Initial string:\n\t%s\n\twith its length of %lu\n", a, strlen(a));
-
-  _compress(a, b);
-  _decompress(b, c);
-
-  return 0;
+    // Initial string length = 34
+    char a[40] = "Hello, my name is Seiya Kobayashi.";
+    // Placeholder for the compressed "a"
+    char b[40];
+    // Placeholder for the uncompressed "b"
+    char c[40];
+    // Prints the initial string (and its length) stored in "a"
+    printf("Initial string:\n\t%s\n\twith its length of %lu\n", a, strlen(a));
+    
+    // Compression(deflation)
+    _compress(a, sizeof(a), b, sizeof(b));
+    // Uncompression(inflation)
+    _uncompress(b, sizeof(b), c, sizeof(c));
+    
+    return 0;
 }
+
